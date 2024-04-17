1d4544a4978908caa91debc98a4428f0fe98c6c8
Andres Carro <acarro@itba.edu.ar>
2024-04-17 20:35:24 -0300

    Add infected visualization mode

--- Visualization/src/main_visualization.py
+++ Visualization/src/main_visualization.py
@@ -15,7 +15,7 @@ L = 0.1
 FAKE_L = 0
 RADIUS_PARTICLES = 0.001
 N = 250
-SPEED = 1
+SPEED = 2
 RADIUS_CIRCLE = 0.005
 # ---------------------------------------------------
 
@@ -29,13 +29,16 @@ WALL_COLOR = (255, 255, 255)
 WALL_THICKNESS = 200
 PARTICLE_COLOR = (255, 0, 0)
 CIRCLE_COLOR = (133, 133, 133)
+INFECTED_COLOR = (0, 0, 255)
 
 
-def complete_visualization_opencv(particles_coords, timeFrames, particle_radius, L, circle_radius, is_movable):
+def complete_visualization_opencv(particles_coords, timeFrames, particle_radius, L, circle_radius, is_movable,
+                                  visualization_mode):
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     SCALED_L = int(L * SCALE_FACTOR)
     video_writer = cv2.VideoWriter(OPENCV_OUTPUT_FILENAME + '.' + MP4_FORMAT, fourcc, 60.0, (SCALED_L,
                                                                                              SCALED_L))
+    infected_map = {}
     for timeFrame in timeFrames:
         current_particle_coords = particles_coords[particles_coords['time'] == timeFrame]
         frame = np.full((SCALED_L, SCALED_L, 3), 255, dtype=np.uint8)
@@ -47,10 +50,20 @@ def complete_visualization_opencv(particles_coords, timeFrames, particle_radius,
 
         for index, fila in current_particle_coords.iterrows():
             current_pos = [int(fila['x'] * SCALE_FACTOR), int(fila['y'] * SCALE_FACTOR)]
-            if is_movable and fila['id'] == N:
+            if is_movable and fila['id'] == N:  # particle id == moving obstacle id
                 cv2.circle(frame, tuple(current_pos), int(circle_radius * SCALE_FACTOR), CIRCLE_COLOR, -1)
-            else:
+            elif visualization_mode != 'infected':
                 cv2.circle(frame, tuple(current_pos), int(particle_radius * SCALE_FACTOR), PARTICLE_COLOR, -1)
+            else:  # visualization_mode == 'infected'
+                if (fila['crash'] == 'NONE' and (fila['ParticleA'] == N or fila['ParticleB'] == N) and
+                        fila['id'] != N and fila['id'] in [fila['ParticleA'], fila['ParticleB']]):  # Has gotten infected
+                    infected_map[fila['id']] = 1
+                    particle_color = INFECTED_COLOR
+                elif infected_map.get(fila['id']) == 1:  # is already infected
+                    particle_color = INFECTED_COLOR
+                else:  # not infected
+                    particle_color = PARTICLE_COLOR
+                cv2.circle(frame, tuple(current_pos), int(particle_radius * SCALE_FACTOR), particle_color, -1)
 
         video_writer.write(frame)
 
@@ -67,11 +80,13 @@ def read_config_file(file_path):
 if __name__ == '__main__':
     config = read_config_file(OUTPUT_PATH + 'StateData_' + str(N) + '_' + str(FAKE_L) + '_' + str(SPEED) + '.json')
     particles_coords = pd.read_csv(PARTICLES_COORDINATES_FILE2)
+    visualization_mode = sys.argv[1]
 
     timeFrames = particles_coords['time'].unique()
     particle_radius = RADIUS_PARTICLES
 
     # OpenCV #
     print('Drawing particles with opencv...')
-    complete_visualization_opencv(particles_coords, timeFrames, particle_radius, L, RADIUS_CIRCLE, config['movable'])
+    complete_visualization_opencv(particles_coords, timeFrames, particle_radius, L, RADIUS_CIRCLE, config['movable'],
+                                  visualization_mode)
     print('DONE!')
